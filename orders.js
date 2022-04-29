368a3301e7afb60c1af44b26cde77f6c907fdcd1
YasarAbi <73652808+YasarAbi@users.noreply.github.com>
2022-04-29 16:37:21 +0300

    Tamamlandı

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Ürün 1',
+        productNumber: '0001',
+        paymentStatus: 'İade Edildi',
+        shipping: 'Reddedildi',
+    },
+    {
+        productName: 'Ürün 2',
+        productNumber: '0002',
+        paymentStatus: 'Ödendi',
+        shipping: 'Tamamlandı',
+    },
+    {
+        productName: 'Ürün 3',
+        productNumber: '0003',
+        paymentStatus: 'Ödendi',
+        shipping: 'Bekleniyor',
+    },
+    {
+        productName: 'Ürün 4',
+        productNumber: '0004',
+        paymentStatus: 'Ödendi',
+        shipping: 'Bekleniyor',
+    },
+    {
+        productName: 'Ürün 5',
+        productNumber: '0005',
+        paymentStatus: 'Ödendi',
+        shipping: 'Bekleniyor',
+    }
+]
